7be6a48de9ebb12d4e9a34c65e3bb1a6cb316205
vit_ru <vctor_r@mail.ru>
2019-08-05 23:54:39 +0300

    Home work 4 try 2

--- app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Profile.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.utils.Utils
+
 data class Profile(
     val firstName: String,
     val lastName: String,
@@ -8,8 +10,9 @@ data class Profile(
     val rating: Int = 0,
     val respect: Int = 0
 ) {
-    val nickName: String = "John Doe" //TODO implement me
+    //val nickName: String = "John Doe" //TODO implement me
     val rank: String = "Junior Android Developer"
+    val nickName: String get() = Utils.transliteration(firstName + "_" + lastName)
 
     fun toMap(): Map<String, Any> = mapOf(
         "nickName" to nickName,
